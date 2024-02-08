f8c6cb494d39440fc8bb50b50e6774b22433483c
Muaaz Abu Zaid <128296319+MuaazAbuZaid@users.noreply.github.com>
2024-02-08 08:41:10 +0200

    Delete Pages directory

--- Pages/About.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st 
-
-st.title('About Title')
-st.write('Paragragh describe the purpose from the project')
-st.write('         1- first idea')
-st.write('         1- second idea')
